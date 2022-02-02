9bdffe52a7a3039af52c19278707f13b56e45843
Hrishikesh Mahajan <mahajanh@ufl.edu>
2022-02-01 23:30:01 -0500

    Added tasks toggling and deletion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-const Header = () => {
-  return (
+import Button from './Button'
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+
+    const onClick = () => {
+        console.log("Click")
+    }
+
+
+    return (
     <div>
-        <h1>Task Tracker</h1>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </div>
   );
 };
+
+Header.defaultProps = {
+    title:'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//Css in JS
+//const headingStyle = {
+//    color:'red',
+//   backgroundColor:'black'
+//}
 
 export default Header;
